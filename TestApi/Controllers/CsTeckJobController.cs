8877f0143daa7e20f3f72ceef682d46e26f1469f
Mohamed abdelaziz <Mohamednosser12345@gmail.com>
2025-08-05 12:40:37 +0300

    Add project files.

---
+++ TestApi/Controllers/CsTeckJobController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+
+namespace TestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CsTeckJobController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCsTeckJob(int E)
+        {
+            if (E <= 0)
+            {
+                return BadRequest("Invalid UserId");
+            }
+            var parameters = new[]
+            {
+                new SqlParameter("@E", E)
+            };
+
+            var executor = new Healper.StoredProcedureExecutor(new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build());
+            var result = executor.ExecuteStoredProcedure("CsTeckJob", parameters);
+            return Ok(result);
+        }
+    }
+}
---
+++ TestApi/Dtos/CsTeckJobDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TestApi.Dtos
+{
+    public class CsTeckJobDto
+    {
+        public string VisitID { get; set; }
+        public DateTime IssueDate { get; set; }
+        public int Mobile { get; set; }
+            
+    }
+}
